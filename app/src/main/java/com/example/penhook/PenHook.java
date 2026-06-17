baa01eb128034ae79a8b46a34fde1a0438770274
纸喵橘柚喵 <3065379460@qq.com>
2026-06-17 20:20:28 +0800

    Add files via upload

--- app/src/main/java/com/example/penhook/PenHook.java
+++ app/src/main/java/com/example/penhook/PenHook.java
@@ -308,6 +308,7 @@ public class PenHook implements IXposedHookLoadPackage {
         @Override public android.view.ActionMode onWindowStartingActionMode(android.view.ActionMode.Callback callback) { return original.onWindowStartingActionMode(callback); }
         @Override public android.view.ActionMode onWindowStartingActionMode(android.view.ActionMode.Callback callback, int type) { return original.onWindowStartingActionMode(callback, type); }
         @Override public boolean dispatchPopulateAccessibilityEvent(android.view.accessibility.AccessibilityEvent event) { return original.dispatchPopulateAccessibilityEvent(event); }
+        @Override public boolean dispatchKeyShortcutEvent(android.view.KeyEvent event) { return original.dispatchKeyShortcutEvent(event); }
     }
 
     private void log(String msg) {
